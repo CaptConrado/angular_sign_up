17586aca9f6d123289655d28b0cc60d6b0616098
CaptConrado <ottey001@gmail.com>
2013-10-09 13:56:37 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finalfrontier::Application.config.secret_token = '40b8980f7b479277fe981e0e8b6d58ce4b07b0056fac4b3e3043413eba00d06bf7f78f32ffdb3fd96c182c2df2aa9a8c843624a13bdbaee16a7c5b241bd3b8a8'
---
+++ test/unit/helpers/enlist_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class EnlistHelperTest < ActionView::TestCase
+end
